50b26c5880a1414ce7d16d314ae2c08e241212dd
JosePainP <67244047+ThePain99@users.noreply.github.com>
2022-08-17 23:08:38 -0500

    first commit

---
+++ src/main/java/com/interaction/interactionsystemwoman/InteractionSystemWomanApplication.java
@@ -0,0 +1,13 @@
+package com.interaction.interactionsystemwoman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractionSystemWomanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractionSystemWomanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interaction/interactionsystemwoman/InteractionSystemWomanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interaction.interactionsystemwoman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractionSystemWomanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
